f58fb70d9f86282ee13a46dbb8aab48f0065bdd1
KumanoTanaka <hiroki.tanaka@kumano-te.com>
2018-04-20 13:17:39 +0900

    refs #1 add bip39 and update license & readme.

--- bip39/bip39_test.go
+++ bip39/bip39_test.go
@@ -5,7 +5,6 @@ import (
 
 	"encoding/hex"
 
-	"github.com/tyler-smith/go-bip39"
 )
 
 func TestNewSeedWithErrorChecking(t *testing.T) {
@@ -17,10 +16,29 @@ func TestNewSeedWithErrorChecking(t *testing.T) {
 	var password string
 	mnemonic = "task another nasty today believe cattle creek apple thought amused cabbage exact clog hurt few radio skate hurt dutch wasp lunch answer snake save"
 	password = ""
-	seed, err := bip39.NewSeedWithErrorChecking(mnemonic, password)
+	seed, err := NewSeedWithErrorChecking(mnemonic, password)
 	if err != nil {
 		t.Error(err)
 	}
+
+	expected = "adf82588662959446c460e159b97e39cf43559d89122a49960d33ca0fbcb1f8369230c3da4977284498cc5463141b85c0e3661d3779d6e4ad3b116be6e2bfeb4"
+	actual = hex.EncodeToString(seed)
+	if actual != expected {
+		t.Errorf("got %v where I want %v", actual, expected)
+	}
+}
+
+
+func TestNewSeed(t *testing.T) {
+
+	var actual interface{}
+	var expected interface{}
+
+	var mnemonic string
+	var password string
+	mnemonic = "task another nasty today believe cattle creek apple thought amused cabbage exact clog hurt few radio skate hurt dutch wasp lunch answer snake save"
+	password = ""
+	seed := NewSeed(mnemonic, password)
 
 	expected = "adf82588662959446c460e159b97e39cf43559d89122a49960d33ca0fbcb1f8369230c3da4977284498cc5463141b85c0e3661d3779d6e4ad3b116be6e2bfeb4"
 	actual = hex.EncodeToString(seed)
